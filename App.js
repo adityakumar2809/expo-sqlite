6740e74a2f6c98253b3d10bb28b04630e02edc86
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-03-03 00:34:59 +0530

    Add List Item Component

--- App.js
+++ App.js
@@ -10,6 +10,49 @@ import {
 import Constants from 'expo-constants';
 import * as SQLite from 'expo-sqlite';
 
+const db = SQLite.openDatabase('db.db');
+
+function Items({ done: doneHeading, onPressItem }) {
+	const [items, setItems] = useState(null);
+
+	useEffect(() => {
+		db.transaction(tx => {
+			tx.executeSql(
+				`select * from items where done = ?;`,
+				[doneHeading ? 1 : 0],
+				(_, { rows: { _array } }) => setItems(_array)
+			);
+		});
+	}, []);
+
+	const heading = doneHeading ? 'Completed' : 'Todo';
+
+	if (items === null || items.length === 0) {
+		return null;
+	}
+
+	return (
+		<View style={styles.sectionContainer}>
+			<Text style={styles.sectionHeading}>{heading}</Text>
+			{items.map(({ id, done, value }) => (
+				<TouchableOpacity
+					key={id}
+					onPress={() => onPressItem && onPressItem(id)}
+					style={{
+						backgroundColor: done ? '#1c9963' : '#fff',
+						borderColor: '#000',
+						borderWidth: 1,
+						padding: 8
+					}}
+				>
+					<Text style={{ color: done ? '#fff' : '#000' }}>{value}</Text>
+				</TouchableOpacity>
+			))}
+		</View>
+	);
+}
+
+
 export default function App() {
 
 	const [text, setText] = useState('');
@@ -43,6 +86,37 @@ export default function App() {
 					value={text}
 				/>
 			</View>
+
+			<ScrollView style={styles.listArea}>
+				<Items
+					key={`forceupdate-todo-${forceUpdateId}`}
+					done={false}
+					onPressItem={id =>
+						db.transaction(
+							tx => {
+								tx.executeSql(`update items set done = 1 where id = ?;`, [
+									id
+								]);
+							},
+							null,
+							forceUpdate
+						)
+					}
+				/>
+				<Items
+					done
+					key={`forceupdate-done-${forceUpdateId}`}
+					onPressItem={id =>
+						db.transaction(
+							tx => {
+								tx.executeSql(`delete from items where id = ?;`, [id]);
+							},
+							null,
+							forceUpdate
+						)
+					}
+				/>
+			</ScrollView>
 
 		</View>
 	);
