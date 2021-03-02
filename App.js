a25a6ccb168bd3f0db5122d89d2d9278bcc3a51d
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-03-03 00:22:07 +0530

    Add Stylesheet and necessary imports

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+import Constants from 'expo-constants';
+import * as SQLite from 'expo-sqlite';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    paddingTop: Constants.statusBarHeight
   },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  flexRow: {
+    flexDirection: 'row'
+  },
+  input: {
+    borderColor: '#4630eb',
+    borderRadius: 4,
+    borderWidth: 1,
+    flex: 1,
+    height: 48,
+    margin: 16,
+    padding: 8
+  },
+  listArea: {
+    backgroundColor: '#f0f0f0',
+    flex: 1,
+    paddingTop: 16
+  },
+  sectionContainer: {
+    marginBottom: 16,
+    marginHorizontal: 16
+  },
+  sectionHeading: {
+    fontSize: 18,
+    marginBottom: 8
+  }
 });
